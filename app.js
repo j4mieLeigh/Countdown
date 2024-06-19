7ea25df64796dc428abc36ac637f4c4cf9d7a5a1
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-06-19 15:36:31 +0200

    Drafted js function to ensure date is always ~10 days in the future

--- app.js
+++ app.js
@@ -27,9 +27,14 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2024, 11, 24, 11, 30, 0);
-console.log(futureDate);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+// let futureDate = new Date(2024, 11, 24, 11, 30, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
